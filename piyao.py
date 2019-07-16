dcaa43a95c362c8aedeb7756e9aff8e4ef9f4897
alina <614915939@qq.com>
2019-07-16 18:38:06 +0800

    newest

--- piyao.py
+++ piyao.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
-import platform
 import time
+import platform
 import os
 import re
 from lxml import etree
-
 
 chrome_driver_path = ""
 driver = None
@@ -31,19 +30,34 @@ driver = webdriver.Chrome(chrome_options=chrome_options,
 driver.get(main_page_url)
 time.sleep(1)
 
-for i in range(3):
+for i in range(7):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight; return lenOfPage;")
-    time.sleep(3)
+    time.sleep(2)
+
+d=[]
+ti=[]
+com=[]
 
 dates=driver.find_elements_by_xpath('//div[@class="day_date"]')
-for date in dates:
-    print(date.text)
-    titles=driver.find_elements_by_xpath('//div[@class="left_title"]')
-    nums=driver.find_elements_by_xpath('//div[@class="comment_text"]')
+for i,date in enumerate(dates):
+    titles=driver.find_elements_by_xpath('//div[@class="zy_day" and position()= %d]/div[@class="day_date"]/following-sibling::ul//div[@class="left_title"]'%(i+1))
+    nums=driver.find_elements_by_xpath('//div[@class="zy_day" and position()= %d]/div[@class="day_date"]/following-sibling::ul//div[@class="comment_text"]'%(i+1))
     for t in titles:
-        print(t.text)
+        d.append(date.text)
+        ti.append(t.text)
+        #print(date.text,t.text)
     for i in nums:
-        print(i.text)
+        com.append(i.text)
+        #print(i.text)
 
+coms = [int(x)for x in com]
 
+list = zip(d,ti,coms)
+newlist = sorted(list, key=lambda x : x[2])
+# print("按照谣言评论数排序：")
+# for i in newlist:
+#     print(i)
+print("捉谣记十大谣言：")
+for x in newlist[-1:-11:-1]:
+    print("日期：",x[0],'\t',"事件:",x[1],'\t',"评论数:",x[2],'\t')
 quit()
